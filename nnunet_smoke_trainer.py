73f0c4d08ec0936fcaa05f64facd8a32a3b954c6
Atharva Dhumal <atharvapdhumal@gmail.com>
2026-04-16 14:43:38 -0700

    Fix nnUNetSmokeTrainer __init__ signature for nnU-Net 2.7.0

--- nnunet_smoke_trainer.py
+++ nnunet_smoke_trainer.py
@@ -2,7 +2,7 @@ from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 
 
 class nnUNetSmokeTrainer(nnUNetTrainer):
-    def __init__(self, plans, configuration, fold, dataset_json, unpack_dataset=True, device=None):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+    def __init__(self, plans, configuration, fold, dataset_json, device=None):
+        super().__init__(plans, configuration, fold, dataset_json, device)
         self.num_epochs = 3
         self.save_every = 1
